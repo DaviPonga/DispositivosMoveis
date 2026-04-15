103b1546f6dfd0afd6873c13610dc61191a6f9f1
DaviPonga <davips2019@gmail.com>
2026-04-15 12:09:22 -0300

    TrocaTela

---
+++ app/src/main/java/com/example/meuapp/Atividade2.java
@@ -0,0 +1,34 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Atividade2 extends AppCompatActivity {
+    Button bntFechar;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_atividade2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        intent i= getIntent();
+        Bundle b=i
+        bntFechar=findViewById(R.id.bntFechar);
+        bntFechar.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et;
+        Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.app_name);
+        et = findViewById(R.id.EditText)
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+
+           Intent i =new Intent(getApplicationContext(),Atividade2.class);
+           String mensagem=et.getText().toString();
+           i.putExtra("msg",msg);
+           startActivity(i);
+
+            });
         }
-    }
+}
