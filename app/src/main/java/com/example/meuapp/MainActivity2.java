8b22acf7fc3d8759040ec68e3b4018e927382299
DaviPonga <davips2019@gmail.com>
2026-04-30 12:02:17 -0300

    Lista

---
+++ app/src/main/java/com/example/meuapp/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button Voltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        Voltar = findViewById(R.id.Voltar);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        Voltar.setOnClickListener(v -> finish());
+        String nome = getIntent().getExtras().getString("nome");
+        TextView tv=findViewById(R.id.textView);
+        tv.setText(nome);
+    }
+}
