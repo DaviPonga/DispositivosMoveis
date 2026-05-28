dffa8d6ee97196b2b46c9da3fd9dbb33ca64d59f
DaviPonga <davips2019@gmail.com>
2026-05-28 12:07:39 -0300

    NovaAtt

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -6,8 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,46 +22,55 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton ib;
+
+    EditText editText;
     SQLiteDatabase sqLiteDatabase;
+    ArrayAdapter<String> adapter;
+    ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        sqLiteDatabase = openOrCreateDatabase("notas",MODE_PRIVATE,null);
+        sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        editText = findViewById(R.id.editTextText);
+        ib = findViewById(R.id.imageButton);
+
+        ib.setOnClickListener(v -> {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("titulo", editText.getText().toString());
+            contentValues.put("nota", editText.getText().toString());
+            sqLiteDatabase.insert("notas", null, contentValues);
+            listagem();
+        });
+        lv = findViewById(R.id.listview);
 
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT,titulo TEXT,nota TEXT)");
-        //String titulo="João";
-        //sqLiteDatabase.execSQL("INSERT INTO notas VALUES(1,'"+titulo+"','')");
-
-
-        /* nomeColuna, valor */
-        ContentValues contentValues= new ContentValues();
-        contentValues.put("titulo","joão");
-        contentValues.put("nota","7777777777");
-        sqLiteDatabase.insert("notas",null,contentValues);
-        ListView lv= findViewById(R.id.listview);
-
-        //Recuperar Dados do SQLITE
-        Cursor c=sqLiteDatabase.rawQuery("SELECT * FROM notas",null);
-        c.moveToFirst();//movimenta o ponteiro do cursor para o primeiro registro recuperado
-        ArrayList<Nota> listaNotas = new ArrayList<>();
-        while(!c.isAfterLast()) {
-            @SuppressLint("Range") int id = c.getInt(c.getColumnIndex("id"));
-            @SuppressLint("Range") String titulo = c.getString(c.getColumnIndex("titulo"));
-            @SuppressLint("Range") String nota = c.getString(c.getColumnIndex("nota"));
-            Nota n = new  Nota(id,titulo,nota);
-            listaNotas.add(n);
-            Log.d("SELECT", Integer.toString(id) + "," + titulo + "," + nota);
-            c.moveToNext();
-        }
-        ArrayList<String> listaTitulos = new ArrayList<>();
-        for(Nota nota:listaNotas){
-            listaTitulos.add(nota.titulo);
-        }
-        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listaTitulos);
-        lv.setAdapter(adapter);
-
-
+        listagem();
     }
-}
+        public void listagem () {
+            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM notas", null);
+            c.moveToFirst();//movimenta o ponteiro do cursor para o primeiro registro recuperado
+            ArrayList<Nota> listaNotas = new ArrayList<>();
+            while (!c.isAfterLast()) {
+                @SuppressLint("Range") int id = c.getInt(c.getColumnIndex("id"));
+                @SuppressLint("Range") String titulo = c.getString(c.getColumnIndex("titulo"));
+                @SuppressLint("Range") String nota = c.getString(c.getColumnIndex("nota"));
+                Nota n = new Nota(id, titulo, nota);
+                listaNotas.add(n);
+                Log.d("SELECT", Integer.toString(id) + "," + titulo + "," + nota);
+                c.moveToNext();
+            }
+            ArrayList<String> listaTitulos = new ArrayList<>();
+            for (Nota nota : listaNotas) {
+                listaTitulos.add(nota.titulo);
+            }
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listaTitulos);
+            lv.setAdapter(adapter);
+
+
+        }
+    }
